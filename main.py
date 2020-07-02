47ea96c0cb9927dbb9bc0c6e926d89b48f8dc47f
cohitai <cohitai@gmail.com>
2020-07-02 16:48:53 +0200

    2.7.2020, 16:48
    Model seems ready, yet to argparser
    - Argparser editing.
    
    * Clone repository to personal laptop.

--- main.py
+++ main.py
@@ -1,86 +1,91 @@
-import XML_reader as XmlReader
 import webscrapper as scrapper
-import preprocessing as pp
 import LivingdocsApi as Liv
 import word2vecmodel as w2v
 import similarity_functions as aux
 import visualization as vis
+import argparse
 
-""" 0. XML_reader. (Object) 
-    1. webscrapper. generating a data frame from blz website. (Object)
+""" 1. webscrapper. generating a data frame from blz website. (Object)
     2. preprocessing. (Module)
     3. word2vecmodel. (Object)
     4. similarity. (Object)
     5. visualization. (Object)
     6. LivingsdocsApi. (Object) download server of Livingdocs into an sql file. """
 
-# Converting files (obsolete ).
 
-# XmlReader.XML_reader()
+def main():
 
-# LivingsdocsApi: creating database.
+    parser = argparse.ArgumentParser(description="berliner zeitung recommendation engine")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-L", "--livingdocs", action="store_true")
+    group.add_argument("-B", "--scrap the website", action="store_true")
+    parser.add_argument("-train", help="train model")
+    parser.add_argument("-load", help="load model from file")
+    args = parser.parse_args()
+    # LivingsdocsApi: creating database.
 
-Li = Liv.LivingDocs()
-Li.initiate_paths(log_file_path="/home/blz/Desktop/output/sources3.csv",
-                  source_path='/home/blz/Desktop/1/', target_path='/home/blz/Desktop/2/')
-# Li.update_server()
+    li = Liv.LivingDocs()
+    li.initiate_paths(log_file_path="/home/blz/Desktop/output/sources3.csv",
+                      source_path='/home/blz/Desktop/1/', target_path='/home/blz/Desktop/2/')
 
-# Li.transform()
-# Li.sql_transform("sqldatabase.db")
+    # Li.update_server()
+    # Li.transform()
+    # Li.sql_transform("sqldatabase.db")
 
-# Web Scrapping.
+    # Web Scrapping.
 
-#scrapper = scrapper.WebScrapper()
-#df = scrapper.create_df(save=True)
+    blz_scrapper = scrapper.WebScrapper()
+    # df = scrapper.create_df(save=True)
+    # load an existing web scrapping data frame.
+    df = blz_scrapper.load_data_frame()
 
-# load an existing web scrapping data frame.
-df = pp.load_data_frame()
+    # Modeling (w2v model)
 
-# Modeling (word2vecmodel)
+    model = w2v.W2V("/home/blz/Desktop/output/sqldatabase.db")
+    model.load_model("/home/blz/Desktop/output/models/model_2020-07-02-10:12:15.model")
 
-# create new model with parameters: embedding size, window size, min count, workers.
+    # create new model with parameters: embedding size, window size, min count, workers.
+    # model.fit(500, 20, 5, 4)
 
-model = w2v.W2V("/home/blz/Desktop/output/sqldatabase.db")
+    # print(model.model.wv.vocab.keys())
+    # print(model.model.wv.vectors.shape[0])
 
-#model.fit(500, 20, 5, 4)
+    # Similarity
 
-# load a model from file.
+    # instantiate similarity object from an existing model.
 
-model.load_model("/home/blz/Desktop/output/models/model_2020-07-02-10:12:15.model")
+    sim = aux.Similarity(model.model, df)
+    sim.add_average_vector()
 
-#print(model.model.wv.vocab.keys())
-#print(model.model.wv.vectors.shape[0])
+    #print(sim.find_similar_article(111, 5))
+    #print(df["Title"][111])
+    #print(df["Title"][85])
+    #print(df["Title"][91])
+    #print(df["Title"][88])
+    #print(df["Title"][142])
+    #print(df["Title"][114])
 
-# Similarity
+    print(sim.predict(k=5))
 
-# instantiate similarity object from an existing model.
-sim = aux.Similarity(model.model)
+    # Visualization
 
-# compute averages, add to df.
-df = sim.add_average_vector(df)
+    visualizer = vis.Visualization(model.model)
 
-print(sim.find_similar_article(df, 111, 5))
-print(df["Title"][111])
-print(df["Title"][85])
-print(df["Title"][91])
-print(df["Title"][88])
-print(df["Title"][142])
-print(df["Title"][114])
+    # 1
+    # visualizer.plot_pca()
+    # 2
+    # visualizer.plot_tsne()
+    # 3
+    # visualizer.plot_keys_cluster()
+    # 4
+    # visualizer.tsne_3d_plot()
+    # 5
+    # visualizer.plot_average_vectors(sim.df)
+    # 6
+    # visualizer.plot_relative_clusters()
 
-# Visualization
+    # visualizer.plot_all_figures()
 
-visualizer = vis.Visualization(model.model)
-# 1
-# visualizer.plot_pca()
-# 2
-visualizer.plot_tsne()
-# 3
-# visualizer.plot_keys_cluster()
-# 4
-# visualizer.tsne_3d_plot()
-# 5
-# visualizer.plot_average_vectors(df)
-# 6
-#visualizer.plot_relative_clusters()
 
-#visualizer.plot_all_figures()
+if __name__ == "__main__":
+    main()
