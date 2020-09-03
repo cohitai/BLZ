a7dfa7dc33c65115557c8411489983daaa427e3d
cohitai <cohitai@gmail.com>
2020-09-03 18:03:45 +0200

    localhost to GCP ip: 34.123.40.94

--- main.py
+++ main.py
@@ -119,7 +119,8 @@ def main():
     if args.automate:
         print("Starting automation:")
         # url = "http://localhost/uploader"
-        url = "http://34.123.40.94/uploader"
+        # url = "http://34.123.40.94/uploader"
+        url = "https://www.apiblzapp.tk/uploader"
         while True:
             sim.df = blz_scrapper.create_df(save=True)
             sim.add_average_vector()
